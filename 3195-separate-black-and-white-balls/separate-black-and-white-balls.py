f2a6f7d2a985c3f2c08cea3aed30ef463479caf3
Shreejith N S <90208023+shreejithns@users.noreply.github.com>
2023-11-24 11:08:38 +0530

    Time: 121 ms (75.40%) | Memory: 17.6 MB (26.91%) - LeetSync

---
+++ 3195-separate-black-and-white-balls/separate-black-and-white-balls.py
@@ -0,0 +1,10 @@
+class Solution:
+    def minimumSteps(self, s):
+        cnt = 0
+        ans = 0
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == '1':
+                ans += cnt
+            else:
+                cnt += 1
+        return ans
