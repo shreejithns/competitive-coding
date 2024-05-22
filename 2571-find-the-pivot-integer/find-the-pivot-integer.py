2e384b0000a7333bfde0617ad67a33261786577e
Shreejith N S <90208023+shreejithns@users.noreply.github.com>
2024-05-22 19:24:03 +0530

    Time: 28 ms (98.32%) | Memory: 16.6 MB (51.73%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.py
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -1,8 +1,20 @@
 class Solution:
     def pivotInteger(self, n: int) -> int:
-        x = sqrt(n * (n + 1) / 2)
-
-        if x % 1 != 0:
-            return -1
+        i=1
+        j=n
+        res=-1
+        if i==j:   # It is just for example 2 i.e. n = 1 cause it does not enter in below while loop cause i and j are same i.e both are 1 so it doesn't enter the while loop and return -1 but we have to return 1 thats why we use this case.
+            return i
         else:
-            return int(x)
+            s1=0
+            s2=0
+            while(i<=j):
+                if s1<=s2:
+                    s1+=i
+                    i+=1
+                elif s2<=s1:
+                    s2+=j
+                    j-=1
+                if j==i and s1==s2:
+                    res=i
+            return res
