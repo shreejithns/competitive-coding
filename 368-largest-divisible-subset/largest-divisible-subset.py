6e54ed02f9f40cf12508c362f1f635465eb3e41f
Shreejith N S <90208023+shreejithns@users.noreply.github.com>
2025-04-06 20:36:24 +0530

    Time: 183 ms (79.23%) | Memory: 18 MB (59.42%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,26 +1,19 @@
-class Solution:
-    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+class Solution(object):
+    def largestDivisibleSubset(self, nums):
         nums.sort()
-        n = len(nums)
-        dp = [1] * n
-        max_size, max_index = 1, 0
-
-        for i in range(1, n):
+        dp = [1] * len(nums)
+        prev = [-1] * len(nums)
+        maxi = 0
+        for i in range(1, len(nums)):
             for j in range(i):
-                if nums[i] % nums[j] == 0:
-                    dp[i] = max(dp[i], dp[j] + 1)
-                    if dp[i] > max_size:
-                        max_size = dp[i]
-                        max_index = i
-
-        result = []
-        num = nums[max_index]
-        for i in range(max_index, -1, -1):
-            if num % nums[i] == 0 and dp[i] == max_size:
-                result.append(nums[i])
-                num = nums[i]
-                max_size -= 1
-
-        return result
-
-
+                if nums[i] % nums[j] == 0 and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+                    prev[i] = j
+            if dp[i] > dp[maxi]:
+                maxi = i
+        res = []
+        i = maxi
+        while i >= 0:
+            res.append(nums[i])
+            i = prev[i]
+        return res
