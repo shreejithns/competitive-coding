7fedd2433d31debe3f0ac25df60159edaf549c0c
Shreejith N S <90208023+shreejithns@users.noreply.github.com>
2025-11-08 19:11:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.96%) - LeetSync

--- 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.py
+++ 1732-minimum-one-bit-operations-to-make-integers-zero/minimum-one-bit-operations-to-make-integers-zero.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumOneBitOperations(self, n: int) -> int:
-        res = 0
+        ans=0
         while n:
-            res = -res - (n ^ (n - 1))
-            n &= n - 1
-        return abs(res)
+            ans=-ans-(n^(n-1))
+            n&=n-1
+        return abs(ans)    
