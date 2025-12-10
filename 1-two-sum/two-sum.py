b8c6f6d33d1ae13a508ee66d065fed5ad0572b65
Shreejith N S <90208023+shreejithns@users.noreply.github.com>
2025-12-10 10:30:04 +0530

    Time: 1 ms (56.76%) | Memory: 18.8 MB (51.47%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,12 +1,8 @@
-from typing import List
-
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # Dictionary to store number and its index
-        lookup = {}
-        
-        for i, num in enumerate(nums):
+    def twoSum(self, nums, target):
+        num_to_index = {}
+        for index, num in enumerate(nums):
             complement = target - num
-            if complement in lookup:
-                return [lookup[complement], i]
-            lookup[num] = i
+            if complement in num_to_index:
+                return [num_to_index[complement], index]
+            num_to_index[num] = index
