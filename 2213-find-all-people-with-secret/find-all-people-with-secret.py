0c71377a7bdd62f4884dd90b60ca0b98fe0b3a30
Shreejith N S <90208023+shreejithns@users.noreply.github.com>
2025-12-19 11:03:44 +0530

    Time: 397 ms (30.58%) | Memory: 68.8 MB (40.77%) - LeetSync

--- 2213-find-all-people-with-secret/find-all-people-with-secret.py
+++ 2213-find-all-people-with-secret/find-all-people-with-secret.py
@@ -1,33 +1,22 @@
 class Solution:
     def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
-        groups = [i for i in range(n)]
-        groups[firstPerson] = 0
+        g = defaultdict(list)
+        for x, y, t in meetings:
+            g[x].append((y, t))
+            g[y].append((x, t))
 
-        meetings.sort(key=lambda x: x[2])
+        inf = 10**18
+        time = [inf] * n
+        time[0] = time[firstPerson] = 0
+        h = [(0, 0), (0, firstPerson)]
 
-        size = len(meetings)
-        i = 0
-        while i < size:
-            current_time = meetings[i][2]
-            temp = []
-            while i < size and meetings[i][2] == current_time:
-                g1 = self.find(groups, meetings[i][0])
-                g2 = self.find(groups, meetings[i][1])
-                groups[max(g1, g2)] = min(g1, g2)
-                temp.extend([meetings[i][0], meetings[i][1]])
-                i += 1
-            for j in temp:
-                if self.find(groups, j) != 0:
-                    groups[j] = j
+        while h:
+            t, u = heapq.heappop(h)
+            if t > time[u]:
+                continue
+            for v, mt in g[u]:
+                if mt >= t and mt < time[v]:
+                    time[v] = mt
+                    heapq.heappush(h, (mt, v))
 
-        result = []
-        for j in range(n):
-            if self.find(groups, j) == 0:
-                result.append(j)
-
-        return result
-
-    def find(self, groups: List[int], index: int) -> int:
-        while index != groups[index]:
-            index = groups[index]
-        return index
+        return [i for i in range(n) if time[i] < inf]
